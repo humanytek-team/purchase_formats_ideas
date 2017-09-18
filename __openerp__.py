6e40f9afed70c4319e62b390316e7d7b9a02835c
samuel <s.mtz.casillas@gmail.com>
2017-09-18 14:16:29 -0700

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Formatos de Compras',
+    'version': '1.1',
+    'category': 'Ideas',
+    'description': """
+    Este Modulo que crea formatos para las operaciones de compras.
+    """,
+    'author': 'Humanytek',
+    'website': 'http://www.humanytek.com',
+    'depends': ['purchase'],
+    'data': [
+        'report/purchase_order.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
